634f8989c9a380884e836d3c45a117a7ba762071
Dan Philibin <dan@danphilibin.com>
2022-10-23 20:52:28 +0000

    improve scraper error handling

--- src/util/scraper.ts
+++ src/util/scraper.ts
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
-import ogs, { OGData } from "ts-open-graph-scraper";
+import ogs from "ts-open-graph-scraper";
 
 export function useOpenGraph(url: string) {
-  const [og, setOg] = useState<OGData | null>(null);
+  const [og, setOg] = useState<{ title?: string; description?: string } | null>(
+    null
+  );
 
   useEffect(() => {
-    ogs({ url }).then(setOg);
+    ogs({ url })
+      .then((res) => {
+        setOg({
+          title: res?.ogTitle,
+          description: Array.isArray(res?.ogDescription)
+            ? res?.ogDescription[0]
+            : res?.ogDescription,
+        });
+      })
+      .catch(() => {
+        setOg({ title: "", description: "" });
+      });
   }, [url]);
 
-  if (!og) return null;
-
-  return {
-    title: og?.ogTitle,
-    description: Array.isArray(og?.ogDescription)
-      ? og?.ogDescription[0]
-      : og?.ogDescription,
-  };
+  return og;
 }
